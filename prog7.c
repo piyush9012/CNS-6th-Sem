fb3af99bf79f154ec9c3cedcdab6448b57415275
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-08-15 23:42:29 +0530

    Diffie Hellman

--- prog7.c
+++ prog7.c
@@ -1,34 +1,23 @@
 #include <stdio.h>
 #include <math.h>
 
-// Power function to return value of a ^ b mod P
-long long int power(long long int a, long long int b,
-                    long long int P)
-{
-    if (b == 1)
-        return a;
-    else
-        return (((long long int)pow(a, b)) % P);
+long long int power(long long int a, long long int b, long long int P) {
+    if (b == 1) return a;
+    else return (((long long int)pow(a, b)) % P);
 }
 
-// Driver program
-int main()
-{
-    long long int P, G, x, a, y, b, ka, kb;
+int main() { 
+    long long int P, G, x, a, b, y, ka, kb;
 
-    // Take input for prime number P
     printf("Enter the value of P (prime number): ");
     scanf("%lld", &P);
 
-    // Take input for primitive root G
     printf("Enter the value of G (primitive root for P): ");
     scanf("%lld", &G);
 
-    // Take input for Alice's private key a
     printf("Enter the private key for Alice (a): ");
     scanf("%lld", &a);
 
-    // Take input for Bob's private key b
     printf("Enter the private key for Bob (b): ");
     scanf("%lld", &b);
 
@@ -39,13 +28,11 @@ int main()
     printf("The private key a for Alice : %lld\n", a);
     printf("The private key b for Bob : %lld\n\n", b);
 
-    x = power(G, a, P); // gets the generated key for Alice
-    y = power(G, b, P); // gets the generated key for Bob
+    x = power(G, a, P);
+    y = power(G, b, P);
 
-    // Generating the secret key after the exchange
-    // of keys
-    ka = power(y, a, P); // Secret key for Alice
-    kb = power(x, b, P); // Secret key for Bob
+    ka = power(y, a, P);
+    kb = power(x, b, P);
 
     printf("Secret key for Alice: %lld\n", ka);
     printf("Secret key for Bob: %lld\n", kb);
