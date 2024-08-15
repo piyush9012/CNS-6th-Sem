c4dd48a5aa2b24cd48a004f3960e0926e7bb3ef1
Piyush Narayan <121113213+piyush9012@users.noreply.github.com>
2024-08-15 23:30:59 +0530

    Caesar Cipher

--- prog1.c
+++ prog1.c
@@ -3,9 +3,8 @@
 #include <ctype.h>
 
 #define MAX_LEN 256
-#define KEY 3 // Fixed key value
+#define KEY 3
 
-// Function to encrypt the message using Caesar Cipher
 void encrypt(char *message) {
     for (int i = 0; i < strlen(message); i++) {
         if (isalpha(message[i])) {
@@ -13,10 +12,8 @@ void encrypt(char *message) {
             message[i] = (message[i] - offset + KEY) % 26 + offset;
         }
     }
-}
-
-// Function to decrypt the message using Caesar Cipher
-void decrypt(char *message){
+} 
+void decrypt(char *message) {
     for (int i = 0; i < strlen(message); i++) {
         if (isalpha(message[i])) {
             char offset = isupper(message[i]) ? 'A' : 'a';
@@ -30,20 +27,16 @@ int main() {
 
     printf("Enter the message to encrypt: ");
     fgets(message, MAX_LEN, stdin);
-    // Remove newline character if present
     message[strcspn(message, "\n")] = '\0';
 
     char encrypted[MAX_LEN];
     strcpy(encrypted, message);
 
     encrypt(encrypted);
-
     printf("Original message: %s\n", message);
     printf("Encrypted message: %s\n", encrypted);
 
     decrypt(encrypted);
-
     printf("Decrypted message: %s\n", encrypted);
-
     return 0;
 }
